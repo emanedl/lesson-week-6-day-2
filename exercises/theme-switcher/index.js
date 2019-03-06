df38d698312a939b47d8e25d9c7f5f42f2f25ee5
Eman Saud <Eman@Emans-MacBook-Pro.local>
2019-03-06 09:46:34 +0300

    submitting home (JS)
    I really liked JS exercises, thanks much.

--- exercises/theme-switcher/index.js
+++ exercises/theme-switcher/index.js
@@ -3,11 +3,18 @@
 // click event for white button
 
 function switchGray() {
-    // change background to gray
+    // change background to white
+    $('body').css('background-color','grey')
     // change font to white
+    $('.text').css('color','white')
 }
 
 function switchWhite() {
     // change background to white
+    $('body').css('background-color','white')
     // change font to black
+    $('.text').css('color','black')
 }
+
+$('#grayButton').click(switchGray);
+$('#whiteButton').click(switchWhite);
